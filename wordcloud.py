e7afd17e08720471f029ddeef8b0187169588a08
JESUS LEGARDA <java@unicauca.edu.co>
2020-08-03 14:22:49 -0500

    Comentarios en español de algunas secciones

--- wordcloud.py
+++ wordcloud.py
@@ -1,11 +1,12 @@
 # Here are all the installs and imports you will need for your word cloud script and uploader widget
-
-!pip install wordcloud
+#Este es un proyecto final del curso "Crash course on Python" de Google en copursera: https://www.coursera.org/learn/python-crash-course
+#es desarrollado en Jupyter Notebook
+"""!pip install wordcloud
 !pip install fileupload
 !pip install ipywidgets
 !jupyter nbextension install --py --user fileupload
 !jupyter nbextension enable --py fileupload
-
+"""
 import wordcloud
 import numpy as np
 from matplotlib import pyplot as plt
@@ -13,9 +14,10 @@ from IPython.display import display
 import fileupload
 import io
 import sys
-file_contents=open("void.txt")
+file_contents=open("void.txt") #Se debe subir un archivo txt de cualquier libro de tu preferencia
 def calculate_frequencies(file_contents):
     # Here is a list of punctuations and uninteresting words you can use to process your text
+    #En mi caso por ser de habla hispana he usado el cuento "vacío" de Andres Caicedo, escritor caleño (Colombia)
     punctuations = '''!()-[];:'"\,<>./?@#$%^&*_~'''
     uninteresting_words = ["El", "a", "los" "la" "si", "es", "ella", "de", "y", "o", "una", "como", "yo" , "mi", \
      "nosotros", "nuestro", "nuestra", "usted", "su", "suyo", "él", "ella", "ellos", \
@@ -24,6 +26,7 @@ def calculate_frequencies(file_contents):
      "todos", "cualquiera", "ambos", "cada uno", "pocos", "más", "algunos", "tales", "no", "ni", "también", "muy", "pueden ", "será sólo"]
     
     # LEARNER CODE START HERE
+    #Este es el codigo pedido por el proyecto para plotear la nube de palabras
     result={}
     a =file_contents.split()
     for words in a:
@@ -44,7 +47,7 @@ def calculate_frequencies(file_contents):
     return cloud.to_array()
 
     # Display your wordcloud image
-
+#Aquí graficamos la nube de palabras
 myimage = calculate_frequencies(file_contents)
 plt.imshow(myimage, interpolation = 'nearest')
 plt.axis('off')
