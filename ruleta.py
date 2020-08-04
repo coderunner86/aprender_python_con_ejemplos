74a221b851f662b352fdf4ee4f31dd4a181861b7
Jheoshua <java@unicauca.edu.co>
2020-08-04 23:41:48 +0000

    Se añade el script diccionario de cuentos

--- ruleta.py
+++ ruleta.py
@@ -1,12 +1,13 @@
+
 import random 
 
-roulete_number = rand(0,37)
+roulete_number = [range(0,37)]
 
 list1 = [range(0,37)]
 count_res1 = 0
 for i in list1:
-    if list1[i] == roulete_number
+    if list1[i] == roulete_number:
         count_res1=+1
 print("el número {} ha aparecido {} veces".format(roulete_number,count_res1))
         
-
+print("hola")
